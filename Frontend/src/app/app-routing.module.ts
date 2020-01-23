be9d1862586c0117d698ddbc92096c6176e592e2
Itsarapong Rattanasak <savezts2@gmail.com>
2020-01-23 16:52:33 +0700

    Edit White page 404

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ import { AddDataComboboxComponent } from './add-data-combobox/add-data-combobox.
     ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
